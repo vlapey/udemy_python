8ad24f92a24796040690e9fd33f8de441401c759
User <vlad.lapey@gmail.com>
2023-07-24 08:55:08 +0300

    completed listcomp3, listcomp4

--- ListComprehension/ListComp2.py
+++ ListComprehension/ListComp2.py
@@ -1,4 +1,3 @@
-
 answer = [val for val in [1, 2, 3, 4] if val in [3, 4, 5, 6]]
 answer2 = [names[::-1].lower() for names in ["Elie", "Tim", "Matt"]]
 print(answer)
---
+++ ListComprehension/ListComp4.py
@@ -0,0 +1,2 @@
+answer = [val for val in "amazing" if val not in ['a', 'i', 'u', 'e', 'o']]
+print(answer)
