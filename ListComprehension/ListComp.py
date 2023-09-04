ec2efa1efee48fe8d8a491122a961c7599ae7f18
Vladislav <vlad.lapey@gmail.com>
2023-09-04 12:51:16 +0300

    added without vowels variable in ListComp.py

--- ListComprehension/ListComp.py
+++ ListComprehension/ListComp.py
@@ -2,3 +2,6 @@ answer = [name[0] for name in ["Elie", "Tim", "Matt"]]
 print(answer)
 answer2 = [num for num in [1, 2, 3, 4, 5, 6] if num % 2 == 0]
 print(answer2)
+with_vowels = "This is so much fun"
+without_vowels = "".join(char for char in with_vowels if char not in "aoeiu")
+print(without_vowels)
