846688219dbf7d99c37b9f5bc0fc19d4ad135773
Vladislav <vlad.lapey@gmail.com>
2023-09-07 15:50:36 +0300

    completed set methods and started set math

--- Tuples_and_Sets/Set.py
+++ Tuples_and_Sets/Set.py
@@ -6,3 +6,13 @@ cities = ["LA", "LA", "Kyoto", "Florence", "Santiago", "Florence", "LA"]
 print(cities)
 unique_cities = list(set(cities))
 print(unique_cities)
+
+
+set1 = set({1, 2, 3, 4, 5, 6})
+set1.add(10)
+set1.remove(5)
+print(set1)
+new_set = set1.copy()
+print(new_set)
+new_set.clear()
+print(new_set)
