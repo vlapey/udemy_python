f4f3a873ea738f9b47555c5058930eab85ba3a61
Vladislav <vlad.lapey@gmail.com>
2023-09-07 15:41:22 +0300

    completed tuples and sets introduction

---
+++ Tuples_and_Sets/Set.py
@@ -0,0 +1,8 @@
+s = set({1, 2, 3, 4, 5, "a", "b", 23.00556})
+print(s)
+# common use for sets
+
+cities = ["LA", "LA", "Kyoto", "Florence", "Santiago", "Florence", "LA"]
+print(cities)
+unique_cities = list(set(cities))
+print(unique_cities)
---
+++ Tuples_and_Sets/Tuple.py
@@ -0,0 +1,2 @@
+locations = {(39.007, 37.064): "CA"}
+print(locations)
