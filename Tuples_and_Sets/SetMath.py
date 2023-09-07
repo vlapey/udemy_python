a966cff7f04815534dce1bacb4c0c63bdee99df9
Vladislav <vlad.lapey@gmail.com>
2023-09-07 16:13:55 +0300

    completed set math

---
+++ Tuples_and_Sets/SetMath.py
@@ -0,0 +1,4 @@
+math_course = {"Lira", "Kisa", "Lenon", "Garold"}
+biology_course = {"Lira", "Garold", "Entony", "Vasilisa"}
+print(math_course | biology_course)
+print(math_course & biology_course)
