507b9eff66d823d6e003017b7c240ca8074b6273
Vladislav <vlad.lapey@gmail.com>
2023-09-26 16:43:09 +0300

    completed DeckOfCards and changed slices

---
+++ DeckOfCards/DeckOfCards.py
@@ -0,0 +1,46 @@
+from random import shuffle
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+
+class Deck:
+    def __init__(self):
+        suits = ("Hearts", "Diamonds", "Clubs", "Spades")
+        values = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, number):
+        length = self.count()
+        actual = min([length, number])
+        if length == 0:
+            raise ValueError("All cards have been dealt")
+        self.cards = self.cards[:-actual]
+        return self.cards[-actual:]
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, number):
+        return self._deal(number)
+
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError("Only full decks can be shuffled")
+        return shuffle(self.cards)
+
+
+my_deck = Deck()
+my_deck._deal(20)
--- Lists/Slices.py
+++ Lists/Slices.py
@@ -1,2 +1,5 @@
-numbers = [1, 2, 3, 4]
-print(numbers[::-1])
+numbers = [1, 2, 3, 4, 5]
+print(numbers[:-2]) # 1, 2, 3
+print(numbers[-2:]) # 4, 5
+print(numbers[2:])  # 3, 4, 5
+print(numbers[:4])  # 1, 2, 3, 4
