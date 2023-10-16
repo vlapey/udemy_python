409fc6af302e2fe67469b302119f3692be22fc28
Vladislav <vlad.lapey@gmail.com>
2023-10-16 17:23:24 +0300

    wrote doctest for capitalize func in Functions

--- 2nd_part/Functions/Capitalize.py
+++ 2nd_part/Functions/Capitalize.py
@@ -1,4 +1,13 @@
 def capitalize(some_string):
+    """returns first letter capitalized of a string
+    >>> capitalize('work')
+    'Work'
+    >>> capitalize(123)
+    Traceback (most recent call last):
+        ...
+    ValueError: some_string must be string
+    """
+
     return some_string[:1].upper() + some_string[1:]
 
 
