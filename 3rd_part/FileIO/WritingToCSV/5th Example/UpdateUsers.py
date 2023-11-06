fc7c9901efd9ff95ece52f6938635fc7d16f15a1
vlad <vlad.lapey@gmail.com>
2023-11-06 18:22:46 +0300

    added example of UpdateUsers task with csv

---
+++ 3rd_part/FileIO/WritingToCSV/5th Example/UpdateUsers.py
@@ -0,0 +1,21 @@
+from csv import reader, writer
+
+
+def update_users(old_name, old_lastname, name, lastname):
+    with open("users.csv") as file:
+        rows = list(reader(file))
+
+    with open("users.csv", "w") as file:
+        writer_object = writer(file)
+        updated_users = 0
+        for row in rows:
+            if row[0] == old_name and row[1] == old_lastname:
+                writer_object.writerow([name, lastname])
+                updated_users += 1
+            else:
+                writer_object.writerow(row)
+
+        return f"Users updated: {updated_users}."
+
+
+print(update_users("Colt", "Steele", "Sosi", "Bibu"))
